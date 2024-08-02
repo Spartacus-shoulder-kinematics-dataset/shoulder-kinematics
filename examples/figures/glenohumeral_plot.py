b00545c04e1a6cdf734187659023e9bd18ddd872
Ipuch <pierre.puchaud@umontreal.ca>
2024-08-02 18:28:47 -0400

    example

--- examples/figures/glenohumeral_plot.py
+++ examples/figures/glenohumeral_plot.py
@@ -1,36 +1,17 @@
 from spartacus import import_data, DataFrameInterface, DataPlanchePlotting
 
 
-def manual_corrections(sub_df):
-    # ONLY FOR DISPLAYING PURPOSE, EXPECT THIS TO DISAPPEAR ANYTIME SOON
-    corrections = {
-        "Begon et al.": (1, 1, -1),
-        "Bourne et al.": (-1, -1, -1),
-        "Moissenet et al.": (1, -1, 0),
-        "Oki et al.": (-1, -1, 1),
-        "Matsumura et al.": (-1, -1, 1),
-    }
-
-    for article, correction in corrections.items():
-        condition = sub_df["article"] == article
-        dof1 = sub_df["degree_of_freedom"] == 1
-        dof2 = sub_df["degree_of_freedom"] == 2
-        dof3 = sub_df["degree_of_freedom"] == 3
-        sub_df.loc[condition & dof1, "value"] *= correction[0]
-        sub_df.loc[condition & dof2, "value"] *= correction[1]
-        sub_df.loc[condition & dof3, "value"] *= correction[2]
-
-    return sub_df
-
-
 def main():
     export = True
-    mvt = "frontal elevation"
+    mvt = "sagittal plane elevation"
     df = import_data(correction=True)
     sub_df = df[df["humeral_motion"] == mvt]
-    sub_df = manual_corrections(sub_df)
+    sub_df = sub_df[sub_df["article"] != "Henninger et al."]
+    sub_df = sub_df[sub_df["article"] == "Begon et al."]
+    # sub_df = sub_df[sub_df["shoulder_id"] == 1]
     dfi = DataFrameInterface(sub_df)
-    plt = DataPlanchePlotting(dfi, restrict_to_joints=["scapulothoracic"])
+    dfi_rot = dfi.rotational_interface()
+    plt = DataPlanchePlotting(dfi_rot, restrict_to_joints=["glenohumeral"])
     plt.plot()
     plt.update_style()
     plt.show()
