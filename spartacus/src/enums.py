a02d5bf483b0cc4aecce82559b985eb035c14dce
Ipuch <pierre.puchaud@umontreal.ca>
2024-08-05 17:40:17 -0400

    Ludwig not lawrence

--- spartacus/src/enums.py
+++ spartacus/src/enums.py
@@ -24,7 +24,7 @@ class DataFolder(Enum):
     KIJIMA_2015 = Path(__file__).parent.parent / "data" / "#8_Kijima_et_al"
     KIM_2017 = Path(__file__).parent.parent / "data" / "#9_Kim_et_al"
     KONOZO_2017 = Path(__file__).parent.parent / "data" / "#10_Kozono_et_al"
-    LAWRENCE_2014 = Path(__file__).parent.parent / "data" / "#11_Ludewig_et_al"
+    LUDEWIG_2014 = Path(__file__).parent.parent / "data" / "#11_Ludewig_et_al"
     MATSUKI_2011 = Path(__file__).parent.parent / "data" / "#12_Matsuki_et_al"
     MATSUMURA_2013 = Path(__file__).parent.parent / "data" / "#13_Matsumura_et_al"
     MOISSENET = Path(__file__).parent.parent / "data" / "#14_Moissenet_et_al"
@@ -51,7 +51,7 @@ class DataFolder(Enum):
             "#8_Kijima_et_al": cls.KIJIMA_2015,  # "Kijima et al 2015"
             "#9_Kim_et_al": cls.KIM_2017,  # "Kim et al 2017"
             "#10_Kozono_et_al": cls.KONOZO_2017,  # "Kozono et al 2017"
-            "#11_Ludewig_et_al": cls.LAWRENCE_2014,
+            "#11_Ludewig_et_al": cls.LUDEWIG_2014,
             "#12_Matsuki_et_al": cls.MATSUKI_2011,  # "Matsuki et al 2011"
             # "Matsuki et al 2011": cls.MATSUKI_2011,
             # "Matsuki et al 2012": cls.MATSUKI_2012,
@@ -89,7 +89,7 @@ class DataFolder(Enum):
             self.KIJIMA_2015: "Kijima et al.",
             self.KIM_2017: "Kim et al.",
             self.KONOZO_2017: "Kozono et al.",
-            self.LAWRENCE_2014: "Lawrence et al.",
+            self.LUDEWIG_2014: "Ludewig et al.",
             self.MATSUKI_2011: "Matsuki et al.",
             self.MATSUMURA_2013: "Matsumura et al.",
             self.MOISSENET: "Moissenet et al.",
