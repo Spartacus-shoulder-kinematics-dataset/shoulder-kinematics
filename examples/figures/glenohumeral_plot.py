9d4d666cb30b5d5e941b42cfc9acc6c806a0fb21
Ipuch <pierre.puchaud@umontreal.ca>
2024-08-05 15:54:20 -0400

    fix a path

--- examples/figures/glenohumeral_plot.py
+++ examples/figures/glenohumeral_plot.py
@@ -7,7 +7,7 @@ def main():
     df = import_data(correction=True)
     sub_df = df[df["humeral_motion"] == mvt]
     sub_df = sub_df[sub_df["article"] != "Henninger et al."]
-    sub_df = sub_df[sub_df["article"] == "Begon et al."]
+    # sub_df = sub_df[sub_df["article"] == "Begon et al."]
     # sub_df = sub_df[sub_df["shoulder_id"] == 1]
     dfi = DataFrameInterface(sub_df)
     dfi_rot = dfi.rotational_interface()
@@ -17,9 +17,9 @@ def main():
     plt.show()
 
     if export:
-        plt.fig.write_image(f"../plots/{mvt}_scapulothoracic.png")
-        plt.fig.write_image(f"../plots/{mvt}_scapulothoracic.pdf")
-        plt.fig.write_html(f"../plots/{mvt}_scapulothoracic.html", include_mathjax="cdn")
+        plt.fig.write_image(f"../../plots/{mvt}_scapulothoracic.png")
+        plt.fig.write_image(f"../../plots/{mvt}_scapulothoracic.pdf")
+        plt.fig.write_html(f"../../plots/{mvt}_scapulothoracic.html", include_mathjax="cdn")
 
 
 if __name__ == "__main__":
