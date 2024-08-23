e3f85277d44820684fdc279f8c2b8d5c5e3edb52
Ipuch <pierre.puchaud@umontreal.ca>
2024-08-23 09:36:42 -0400

    fix: yes DATASET.CLEAN doesnt exist anymore

--- spartacus/quick_load.py
+++ spartacus/quick_load.py
@@ -9,7 +9,7 @@ def import_data(correction: bool = True):
     """Import the data from the confident_data.csv file if it exists, otherwise it's computed from the raw data."""
     file = "corrected_confident_data.csv" if correction else "confident_data.csv"
 
-    if "confident_data.csv" in os.listdir(str(Path(DatasetCSV.CLEAN.value).parent)):
-        return pd.read_csv(Path(DatasetCSV.CLEAN.value).parent / file)
+    if "confident_data.csv" in os.listdir(str(Path(DatasetCSV.JOINT.value).parent)):
+        return pd.read_csv(Path(DatasetCSV.JOINT.value).parent / file)
     else:
         raise ValueError("The confident_data.csv file does not exist. You must run the correction first.")
